98024591fcf736a9d93c24a9afae38c71cb09b6a
Sebastian Müller <mueller@open4business.de>
2023-09-04 20:54:24 +0200

    Add example /api/demo endpoint

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,22 @@
-<script setup>
+<script setup lang="ts">
 const buttonClickCount = ref(0)
 const buttonClickCountOptionsAPI = ref(0)
 
-function buttonWasClicked (newCount) {
+const fetchData = await $fetch('/api/demo')
+const { data } = await useFetch('/api/demo')
+
+function buttonWasClicked (newCount : number) {
   buttonClickCount.value = newCount
 }
 
-function buttonWasClickedOptionsAPI (newCount) {
+function buttonWasClickedOptionsAPI (newCount : number) {
   buttonClickCountOptionsAPI.value = newCount
 }
+
+onMounted(() => {
+  console.log('Data from /api/hello using $fetch:', fetchData)
+  console.log('Data from /api/hello using useFetch:', data)
+})
 </script>
 
 <template>
@@ -30,6 +38,15 @@ function buttonWasClickedOptionsAPI (newCount) {
     <BaseButtonOptionsAPI @button-clicked="buttonWasClickedOptionsAPI">
       Click me!
     </BaseButtonOptionsAPI>
+  
     Button click count: {{ buttonClickCountOptionsAPI }}
+
+    <p>Data from /api/hello endpoint:</p>
+    <div class="w-1/2 grid grid-cols-4" v-for="(person, index) in data" :key="index">
+      <div class="bg-teal-500 text-white p-2">gender: {{ person.gender }}</div>
+      <div class="bg-amber-500 text-white p-2">firstName: {{ person.firstName }}</div>
+      <div class="bg-yellow-500 text-white p-2">lastName: {{ person.lastName }}</div>
+      <div class="bg-lime-500 text-white p-2">age: {{ person.age }}</div>
+    </div>
   </div>
 </template>
